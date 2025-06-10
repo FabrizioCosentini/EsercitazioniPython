211a6d6c41c77762cc237900e4a7f15ed1e2cb20
FabrizioCosentini <fabrizio.cosentini.2006@gmail.com>
2025-06-10 15:51:11 +0200

    task: print, input, f-string

---
+++ Esercitazione1.py
@@ -0,0 +1,18 @@
+#Task on input and print commands
+#Verb1=input("Write a verb: ")
+#print("I can", Verb1, "better than you!")
+#print (4*(Verb1+" ")+Verb1)
+
+#Task on Newton's Method (on print and input)
+#x=int(input("WHat x to find the cube root of? "))
+#g=int(input("What guess to start with? "))
+#print("Current estimate cubed = ", g**3)
+#next_g=g-((g**3-x)/(3*g**2))
+#print("Next guess to try = ", next_g)
+
+#f-strings task
+#num=3000
+#fraction=1/3
+#print(num*fraction, "is", fraction*100, "% of", num)
+#print( num*fraction, "is", str(fraction*100) + "% of", num)
+#print(f"{num*fraction} is {fraction*100}% of {num}")
